0d79ccff484db85a710627e99244f52e30e5b096
Walid OUMOUZOUNE <73303972+WalidOumouzoune@users.noreply.github.com>
2022-06-24 01:40:58 +0100

    LastUpload

---
+++ GYM_Template/js/main.js
@@ -0,0 +1,29 @@
+function fnBrowserDetect() {
+    let userAgent = navigator.userAgent;
+    let browserName;
+    if (userAgent.match(/firefox|fxios/i)) {
+        browserName = "firefox";
+        document.querySelectorAll('p').forEach(e => e.style.opacity = "0.8");
+    }
+}
+
+import ScrollTop from 'https://cdn.skypack.dev/smooth-scroll-top';
+const scrollTop = new ScrollTop();
+scrollTop.init();
+
+import confetti from "https://cdn.skypack.dev/canvas-confetti";
+window.party = function () {
+    confetti();
+};
+
+window.addEventListener('scroll', function () {
+    let header = document.querySelector('header');
+    let windowPosition = window.scrollY > 0;
+    header.classList.toggle('scrolling-active', windowPosition);
+})
+
+
+function goBackToLastPage() {
+    window.history.go(-1)
+}
+
